71dd2065ee5a06f58b8b96e4c28f3d95a0843f9a
larrylebron <larrylebron@13bcb498-b18b-6dd3-a6a6-4201be5eb42a>
2013-03-04 00:29:18 +0000

    Logic manager merged.

--- trunk/Assets/scripts/LogicManager.cs
+++ trunk/Assets/scripts/LogicManager.cs
@@ -1,14 +1,47 @@
 using UnityEngine;
 using System.Collections;
-using System;
 using System.IO;
+using System;
 
 public class LogicManager : MonoBehaviour {
 	
-	public string[] scenes = new string[20];
+	public GUIManager gui;
 	
+	//How many seconds in the game?
+	private float gameLength = 300;
+	
+	//how many seconds per choice?
+	private int choiceLength = 15;
+	
+	//when did the current choice start?
+	private float choiceStartTime;
+		
+	//the time the game has been running
+	private float gameTimeRemaining;
+	public float GameTimeRemaining{
+		get {return gameTimeRemaining;}
+	}
+	
+	//how long until the current choice is selected?
+	private int choiceTimeRemaining;
+	public int ChoiceTimeRemaining {
+		get {return choiceTimeRemaining;}
+	}
+	
+	//how many choices
+	private int numChoices = 3;
+	
+	//the array of choice options 
+	private string[] choiceStrings;	
+	
+	public string[] scenes = new string[20];
+	public IList[] choices = new IList[20];
 	
 	void Start () {
+		choiceStartTime = Time.timeSinceLevelLoad; //initialize the choice timer
+		choiceStrings = new string[numChoices];
+		initChoiceStrings();//initialize the choice strings
+		
 		try {			
 			// Create an instance of StreamReader to read from a file. 
             // The using statement also closes the StreamReader. 
@@ -38,11 +71,38 @@ public class LogicManager : MonoBehaviour {
             Debug.Log("The file could not be read:");
             Debug.Log(e.Message);
 		}
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		gameTimeRemaining = gameLength - Time.timeSinceLevelLoad;
+		handleChoiceTimer();
+	}
 	
+	void initChoiceStrings() {
+		for (int i = 0; i < numChoices; i++) {
+			choiceStrings[i] = ("logicChoice " + i);
+		}
+		gui.setChoices(choiceStrings);//set the choices in the gui
+	}
+	
+	/*
+	 * update the choice time
+	 * If time's up, handle the choice
+	 * */
+	void handleChoiceTimer() {
+		float currTime = Time.timeSinceLevelLoad;
+		float choiceTimePassed = currTime - choiceStartTime;
+		
+		if(choiceTimePassed >= choiceLength) {
+			choiceTimeRemaining = choiceLength;
+			choiceStartTime = currTime;
+			handleChoice();
+		} else choiceTimeRemaining = choiceLength - (int)choiceTimePassed;
+	}
+	
+	//handle a user choice
+	void handleChoice() {
+		//set new choices here after dealing with repercussions
 	}
 }
