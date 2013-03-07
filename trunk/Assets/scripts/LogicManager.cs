2447aa2162777642719b1ab788f1395299d60879
pmaddaloni@gmail.com <pmaddaloni@gmail.com@13bcb498-b18b-6dd3-a6a6-4201be5eb42a>
2013-03-07 01:29:13 +0000

    Updates for LogicManager and Parser.cs

--- trunk/Assets/scripts/LogicManager.cs
+++ trunk/Assets/scripts/LogicManager.cs
@@ -4,16 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System;
 
-//holds all the information in a choice
-public struct choiceNode {
-		public string label;
-		public string description;
-		public string successText;
-		public string failureText;
-		public int impactAmount;
-		public float challengeRate;
-	}
-
 public class LogicManager : MonoBehaviour
 {
 	
@@ -69,23 +59,170 @@ public class LogicManager : MonoBehaviour
 	//the array of choice options 
 
 	private choiceNode[] choices;
+	public struct choiceNode {
+		public string label;
+		public string description;
+		public string successText;
+		public string failureText;
+		public int impactAmount;
+		public float challengeRate;
+	}
+			
 	public string[] scenes = new string[20];
 	public List<choiceNode>[] choiceList = new List<choiceNode>[6];
 	public string sceneText; //the scene description
+	
+	private int currentChoiceClass = 0;
 
-	
-	
 	//---------------------------METHODS-----------------------------------------//	
 	void Start () {
 		choiceStartTime = Time.timeSinceLevelLoad; //initialize the choice timer
 		choices = new choiceNode[numChoices];
-		Parser parser = gameObject.AddComponent<Parser>();
-		parser.parseScenes("Scenes.txt", scenes);
-		parser.parseChoices("Choices.txt", choiceList);
-	
-		sceneText = scenes[0];
 		
-		TensionManager tension = gameObject.AddComponent<TensionManager>();
+		try {			
+			// Create an instance of StreamReader to read from a file. 
+            // The using statement also closes the StreamReader. 
+			
+			//Read Scenes
+            using (StreamReader sr = new StreamReader("Scenes.txt")) 
+            {
+                string line;
+				int sceneCounter = 0;
+                // Read and display lines from the file until the end of  
+                // the file is reached. 
+                while ((line = sr.ReadLine()) != null) 
+                {
+                    //Debug.Log(line);
+					if ( (line.Trim().ToUpper()).Equals("<INTRO>") )
+					{
+						line = sr.ReadLine();
+						while( !(line.Trim().ToUpper()).Equals("<END>") )
+						{
+							scenes[sceneCounter] += line + '\n';
+							line = sr.ReadLine();
+						}
+						sceneCounter++;
+					}
+					
+					else {
+						line = sr.ReadLine();						
+						if ((line.Trim().ToUpper()).Equals("<SCENE" + sceneCounter + ">"))
+						{
+	
+							line = sr.ReadLine();
+							while( !(line.Trim().ToUpper()).Equals("<END>") && sceneCounter < 20)
+							{
+								scenes[sceneCounter] += line;
+								line = sr.ReadLine();
+							}
+							sceneCounter++;
+						}			
+					}
+                }
+            }
+			
+			//Read Choices
+			using (StreamReader sr = new StreamReader("Choices.txt")) 
+            {
+				int classCounter = 0;
+                string line;
+                // Read and display lines from the file until the end of  
+                // the file is reached. 
+                while ((line = sr.ReadLine()) != null) 
+                {
+					if ((line.Trim()).Length == 0)
+					{	
+						continue;
+					}
+					
+					if ( (line.Trim()).ToUpper().Equals("<CLASS" + classCounter +">") )
+					{
+						choiceList[classCounter] = new List<choiceNode>();			
+						while( (line = sr.ReadLine()) != null && !(line.Trim()).ToUpper().Equals("<ENDCLASS>") )
+						{
+							choiceNode temp = new choiceNode();
+							
+							if ((line.Trim()).Length == 0)
+							{	
+								continue;
+							}
+							
+							while( (line = sr.ReadLine()) != null && !(line.Trim()).ToUpper().Equals("<ENDCHOICE>") )
+							{			
+								if ((line.Trim()).Length == 0)
+								{	
+									continue;
+								}
+								switch((line.Trim()).ToUpper())
+								{
+									
+									case "<LABEL>":
+										line = sr.ReadLine();
+										while( !(line.Trim()).ToUpper().Equals("<END>") )
+										{
+											temp.label += line.Trim() + '\n';
+											line = sr.ReadLine();
+										}
+										break;
+									case "<DESCRIPTION>":
+										line = sr.ReadLine();
+										while( !(line.Trim()).ToUpper().Equals("<END>") )
+										{
+											temp.description += line.Trim() + '\n';
+											line = sr.ReadLine();
+										}
+										break;
+									case "<SUCCESS>":
+										line = sr.ReadLine();
+										while( !(line.Trim()).ToUpper().Equals("<END>") )
+										{
+											temp.successText += line.Trim () + '\n';
+											line = sr.ReadLine();
+										}
+										break;
+									case "<FAILURE>":
+										line = sr.ReadLine();
+										while( !(line.Trim()).ToUpper().Equals("<END>") )
+										{
+											temp.failureText += line.Trim() + '\n';
+											line = sr.ReadLine();
+										}
+										break;
+									case "<IMPACT>":
+										line = sr.ReadLine();
+										while( !(line.Trim()).ToUpper().Equals("<END>") )
+										{
+											temp.impactAmount = Convert.ToInt32(line.Trim());
+											line = sr.ReadLine();
+										}
+										break;
+									case "<CHALLENGE>":
+										line = sr.ReadLine();
+										while( !(line.Trim()).ToUpper().Equals("<END>") )
+										{
+											temp.challengeRate = Convert.ToInt32(line.Trim());
+											line = sr.ReadLine();
+										}
+										break;
+									default:
+										break;
+								}
+							}
+							choiceList[classCounter].Add(temp);
+						}
+						classCounter++; 
+					}//end if			
+					
+                }//end CLASS while loop
+            }//end using
+			sceneText = scenes[0];
+		}
+		catch (Exception e)
+		{
+            // Let the user know what went wrong.
+            Debug.Log("The file could not be read:");
+            Debug.Log(e.Message);
+		}
 	}
 	
 	// Update is called once per frame
@@ -105,31 +242,32 @@ public class LogicManager : MonoBehaviour
 	//Start the game -- initialize timers
 	void startGame ()
 	{
+		gui.activateGameGUI();//start up the GUI
 		gameStarted = true;
 		gameStartTime = Time.timeSinceLevelLoad;
 		choiceStartTime = Time.timeSinceLevelLoad;
 		setChoices();//initialize the choices
 		updateGameStatus (); //initialize the game status
-		gui.activateGameGUI();//start up the GUI
 		//seed the random number generator
 		UnityEngine.Random.seed = (int)System.DateTime.Now.TimeOfDay.TotalMilliseconds;
 	}
 	
 	//set the current choices
 	void setChoices() {
-		for (int i = 0; i < numChoices; i++) {
+		List<choiceNode> tmp = choiceList[currentChoiceClass];
+		
+		for (int i = 0; i < numChoices; i++) 
+		{
 			choices[i] = new choiceNode();
+			int choice = UnityEngine.Random.Range(0, tmp.Count);
 			
-			//just using dummy data - the logic for pulling these from the doc and setting levels 
-			//should hapen here
-			choices[i].label = ("choiceNode " + i);
-			choices[i].challengeRate = .5f;
-			choices[i].impactAmount = 1;
-			choices[i].description = "You valiantly try to do thing " + i + "\n";
-			choices[i].successText = "You succeed at thing " + i + ".\n The crazy mofo moves " 
-				+ choices[i].impactAmount + " steps away from the ledge.\n";
-			choices[i].failureText = "Ah, crap. You fail miserably at thing " + i + ".\n The crazy mofo moves " 
-				+ choices[i].impactAmount + " steps towards from the ledge.\n";
+			choices[i].label = tmp[choice].label;
+			choices[i].challengeRate = tmp[choice].challengeRate;
+			choices[i].impactAmount = tmp[choice].impactAmount;
+			choices[i].description = tmp[choice].description;
+			choices[i].successText = tmp[choice].successText;
+			choices[i].failureText = tmp[choice].failureText;
+			tmp.RemoveAt(choice);
 		}
 		setGUIChoiceStrings();
 	}
