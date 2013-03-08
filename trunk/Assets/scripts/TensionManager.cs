d57ccacd9262e7af2fc96ab2045e3f4ebec0feb4
larrylebron <larrylebron@13bcb498-b18b-6dd3-a6a6-4201be5eb42a>
2013-03-08 23:40:52 +0000

    Started on tension manager. Fixed GUI bug.

--- trunk/Assets/scripts/TensionManager.cs
+++ trunk/Assets/scripts/TensionManager.cs
@@ -1,12 +1,32 @@
 using System.Collections;
+using System.Collections.Generic;
 using System;
 using UnityEngine;
 
 public class TensionManager : MonoBehaviour {
-
+	
+	private float arcDuration;//how long the experience should be, in seconds
+	private List<float> tensionLevels; //a list of tension levels, to be evenly distributed over the arcDuration
+	
+	bool initialized = false;
+	
 	public void Start() {
+		
 	}
 	
 	public void Update() {
+		if(!initialized) {
+			Debug.Log ("Tension Manager not initialized");
+		}
+	}
+	
+	public void init(float duration, string tensionFileName) {
+		if(initialized) {
+			Debug.Log("Tried to re-initialize a tensionManager");
+			return;
+		}
+		
+		initialized  = true;
+		arcDuration = duration;
 	}
 }
