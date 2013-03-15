772f1dc036b2ef26cdceb2be64ae59fb22919b7e
pmaddaloni <pmaddaloni@13bcb498-b18b-6dd3-a6a6-4201be5eb42a>
2013-03-15 22:16:46 +0000

    Updates for random events being either positive or negative.

--- Assets/scripts/Parser.cs
+++ Assets/scripts/Parser.cs
@@ -10,45 +10,37 @@ public class Parser : MonoBehaviour
 	{
 		try {			
 			// Create an instance of StreamReader to read from a file. 
-            // The using statement also closes the StreamReader. 
+			// The using statement also closes the StreamReader. 
 			
 			//Read Scenes
-            using (StreamReader sr = new StreamReader(fileName)) 
-            {
-                string line;
+			using (StreamReader sr = new StreamReader(fileName)) {
+				string line;
 				int sceneCounter = 0;
-                // Read and display lines from the file until the end of  
-                // the file is reached. 
-                while ((line = sr.ReadLine()) != null) 
-                {
-                    //Debug.Log(line);
-					if ( (line.Trim().ToUpper()).Equals("<INTRO>") )
-					{
-						line = sr.ReadLine();
-						while( !(line.Trim().ToUpper()).Equals("<END>") )
-						{
-							scenes[sceneCounter] += line + '\n';
-							line = sr.ReadLine();
+				// Read and display lines from the file until the end of  
+				// the file is reached. 
+				while ((line = sr.ReadLine()) != null) {
+					//Debug.Log(line);
+					if ((line.Trim ().ToUpper ()).Equals ("<INTRO>")) {
+						line = sr.ReadLine ();
+						while (!(line.Trim().ToUpper()).Equals("<END>")) {
+							scenes [sceneCounter] += line + '\n';
+							line = sr.ReadLine ();
 						}
 						sceneCounter++;
-					}
-					
-					else {
-						line = sr.ReadLine();						
-						if ((line.Trim().ToUpper()).Equals("<SCENE" + sceneCounter + ">"))
-						{
+					} else {
+						line = sr.ReadLine ();						
+						if ((line.Trim ().ToUpper ()).Equals ("<SCENE" + sceneCounter + ">")) {
 	
-							line = sr.ReadLine();
-							while( !(line.Trim().ToUpper()).Equals("<END>") && sceneCounter < 20)
-							{
-								scenes[sceneCounter] += line;
-								line = sr.ReadLine();
+							line = sr.ReadLine ();
+							while (!(line.Trim().ToUpper()).Equals("<END>") && sceneCounter < 20) {
+								scenes [sceneCounter] += line;
+								line = sr.ReadLine ();
 							}
 							sceneCounter++;
 						}			
 					}
-                }
-            }
+				}
+			}
 		} catch (Exception e) {
 			// Let the user know what went wrong.
 			Debug.Log (e.Message);
@@ -58,105 +50,88 @@ public class Parser : MonoBehaviour
 	public void parseChoices (string fileName, List<choiceNode>[] choiceList)
 	{
 		try {
-						//Read Choices
-			using (StreamReader sr = new StreamReader(fileName)) 
-            {
+			//Read Choices
+			using (StreamReader sr = new StreamReader(fileName)) {
 				int classCounter = 0;
-                string line;
-                // Read and display lines from the file until the end of  
-                // the file is reached. 
-                while ((line = sr.ReadLine()) != null) 
-                {
-					if ((line.Trim()).Length == 0)
-					{	
+				string line;
+				// Read and display lines from the file until the end of  
+				// the file is reached. 
+				while ((line = sr.ReadLine()) != null) {
+					if ((line.Trim ()).Length == 0) {	
 						continue;
 					}
 					
-					if ( (line.Trim()).ToUpper().Equals("<CLASS" + classCounter +">") )
-					{
-						choiceList[classCounter] = new List<choiceNode>();			
-						while( (line = sr.ReadLine()) != null && !(line.Trim()).ToUpper().Equals("<ENDCLASS>") )
-						{
-							choiceNode temp = new choiceNode();
+					if ((line.Trim ()).ToUpper ().Equals ("<CLASS" + classCounter + ">")) {
+						choiceList [classCounter] = new List<choiceNode> ();			
+						while ((line = sr.ReadLine()) != null && !(line.Trim()).ToUpper().Equals("<ENDCLASS>")) {
+							choiceNode temp = new choiceNode ();
 							
-							if ((line.Trim()).Length == 0)
-							{	
+							if ((line.Trim ()).Length == 0) {	
 								continue;
 							}
 							
-							while( (line = sr.ReadLine()) != null && !(line.Trim()).ToUpper().Equals("<ENDCHOICE>") )
-							{			
-								if ((line.Trim()).Length == 0)
-								{	
+							while ((line = sr.ReadLine()) != null && !(line.Trim()).ToUpper().Equals("<ENDCHOICE>")) {			
+								if ((line.Trim ()).Length == 0) {	
 									continue;
 								}
-								switch((line.Trim()).ToUpper())
-								{
+								switch ((line.Trim ()).ToUpper ()) {
 									
-									case "<LABEL>":
-										line = sr.ReadLine();
-										while( !(line.Trim()).ToUpper().Equals("<END>") )
-										{
-											temp.label += line.Trim() + '\n';
-											line = sr.ReadLine();
-										}
-										break;
-									case "<DESCRIPTION>":
-										line = sr.ReadLine();
-										while( !(line.Trim()).ToUpper().Equals("<END>") )
-										{
-											temp.description += line.Trim() + '\n';
-											line = sr.ReadLine();
-										}
-										break;
-									case "<SUCCESS>":
-										line = sr.ReadLine();
-										while( !(line.Trim()).ToUpper().Equals("<END>") )
-										{
-											temp.successText += line.Trim () + '\n';
-											line = sr.ReadLine();
-										}
-										break;
-									case "<FAILURE>":
-										line = sr.ReadLine();
-										while( !(line.Trim()).ToUpper().Equals("<END>") )
-										{
-											temp.failureText += line.Trim() + '\n';
-											line = sr.ReadLine();
-										}
-										break;
-									case "<IMPACT>":
-										line = sr.ReadLine();
-										while( !(line.Trim()).ToUpper().Equals("<END>") )
-										{
-											temp.impactAmount = Convert.ToInt32(line.Trim());
-											line = sr.ReadLine();
-										}
-										break;
-									case "<CHALLENGE>":
-										line = sr.ReadLine();
-										while( !(line.Trim()).ToUpper().Equals("<END>") )
-										{
-											temp.challengeRate = Convert.ToInt32(line.Trim());
-											line = sr.ReadLine();
-										}
-										break;
-									default:
-										break;
+								case "<LABEL>":
+									line = sr.ReadLine ();
+									while (!(line.Trim()).ToUpper().Equals("<END>")) {
+										temp.label += line.Trim () + '\n';
+										line = sr.ReadLine ();
+									}
+									break;
+								case "<DESCRIPTION>":
+									line = sr.ReadLine ();
+									while (!(line.Trim()).ToUpper().Equals("<END>")) {
+										temp.description += line.Trim () + '\n';
+										line = sr.ReadLine ();
+									}
+									break;
+								case "<SUCCESS>":
+									line = sr.ReadLine ();
+									while (!(line.Trim()).ToUpper().Equals("<END>")) {
+										temp.successText += line.Trim () + '\n';
+										line = sr.ReadLine ();
+									}
+									break;
+								case "<FAILURE>":
+									line = sr.ReadLine ();
+									while (!(line.Trim()).ToUpper().Equals("<END>")) {
+										temp.failureText += line.Trim () + '\n';
+										line = sr.ReadLine ();
+									}
+									break;
+								case "<IMPACT>":
+									line = sr.ReadLine ();
+									while (!(line.Trim()).ToUpper().Equals("<END>")) {
+										temp.impactAmount = Convert.ToInt32 (line.Trim ());
+										line = sr.ReadLine ();
+									}
+									break;
+								case "<CHALLENGE>":
+									line = sr.ReadLine ();
+									while (!(line.Trim()).ToUpper().Equals("<END>")) {
+										temp.challengeRate = Convert.ToInt32 (line.Trim ());
+										line = sr.ReadLine ();
+									}
+									break;
+								default:
+									break;
 								}
 							}
-							choiceList[classCounter].Add(temp);
+							choiceList [classCounter].Add (temp);
 						}
 						classCounter++; 
 					}//end if			
 					
-                }//end CLASS while loop
-            }//end using
-		}
-		catch (Exception e)
-		{
-            // Let the user know what went wrong.
-            Debug.Log(e.Message);
+				}//end CLASS while loop
+			}//end using
+		} catch (Exception e) {
+			// Let the user know what went wrong.
+			Debug.Log (e.Message);
 		}
 	}
 	
@@ -164,51 +139,57 @@ public class Parser : MonoBehaviour
 	{
 		try {			
 			// Create an instance of StreamReader to read from a file. 
-            // The using statement also closes the StreamReader. 
+			// The using statement also closes the StreamReader. 
 			
 			//Read Scenes
-            using (StreamReader sr = new StreamReader(fileName)) 
-            {
-                string line;
+			using (StreamReader sr = new StreamReader(fileName)) {
+				string line;
 				int randomEventCounter = 0;
-                // Read and display lines from the file until the end of  
-                // the file is reached. 
-                while ((line = sr.ReadLine()) != null) 
-                {
-                    //Debug.Log(line);
-					if ( (line.Trim().ToUpper()).Equals("<RandomScene" + randomEventCounter + ">") )
-					{
-						while( (line = sr.ReadLine()) != null && !(line.Trim()).ToUpper().Equals("<ENDSCENE>") )
-						{			
-								if ((line.Trim()).Length == 0)
-								{	
-									continue;
+				// Read and display lines from the file until the end of  
+				// the file is reached. 
+				while ((line = sr.ReadLine()) != null) {
+					//Debug.Log(line);
+					if ((line.Trim ().ToUpper ()).Equals ("<RandomScene" + randomEventCounter + ">")) {
+						while ((line = sr.ReadLine()) != null && !(line.Trim()).ToUpper().Equals("<ENDSCENE>")) {			
+							if ((line.Trim ()).Length == 0) {	
+								continue;
+							}
+							switch ((line.Trim ()).ToUpper ()) {
+							case "<DESCRIPTION>":
+								line = sr.ReadLine ();
+								while (!(line.Trim()).ToUpper().Equals("<END>")) {
+									randomEvents [randomEventCounter].description += line.Trim () + '\n';
+									line = sr.ReadLine ();
 								}
-								switch((line.Trim()).ToUpper())
-								{
-									case "<DESCRIPTION>":
-										line = sr.ReadLine();
-										while( !(line.Trim()).ToUpper().Equals("<END>") )
-										{
-											randomEvents[randomEventCounter].description += line.Trim() + '\n';
-											line = sr.ReadLine();
-										}
-										break;
-									case "<IMPACT>":
-										line = sr.ReadLine();
-										while( !(line.Trim()).ToUpper().Equals("<END>") )
-										{
-											randomEvents[randomEventCounter].impactAmount = Convert.ToInt32(line.Trim());
-											line = sr.ReadLine();
-										}
-										break;
-									default:
-										break;
+								break;
+							case "<POSITIVE>":
+								line = sr.ReadLine ();
+								while (!(line.Trim()).ToUpper().Equals("<END>")) {
+									randomEvents [randomEventCounter].positiveEvent += line.Trim () + '\n';
+									line = sr.ReadLine ();
 								}
+								break;
+							case "<NEGATIVE>":
+								line = sr.ReadLine ();
+								while (!(line.Trim()).ToUpper().Equals("<END>")) {
+									randomEvents [randomEventCounter].negativeEvent += line.Trim () + '\n';
+									line = sr.ReadLine ();
+								}
+								break;
+							case "<IMPACT>":
+								line = sr.ReadLine ();
+								while (!(line.Trim()).ToUpper().Equals("<END>")) {
+									randomEvents [randomEventCounter].impactAmount = Convert.ToInt32 (line.Trim ());
+									line = sr.ReadLine ();
+								}
+								break;
+							default:
+								break;
+							}
 							randomEventCounter++;
 						}
-	                }
-	            }
+					}
+				}
 			}
 		} catch (Exception e) {
 			// Let the user know what went wrong.
